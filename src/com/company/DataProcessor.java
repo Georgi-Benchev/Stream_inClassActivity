718b3afeec9f8ef5131eaee38abc7a38caf54490
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2024-11-19 15:18:56 +0200

    findTheAverageAgeOfPeopleWhoDislikeMovies method with stream

--- src/com/company/DataProcessor.java
+++ src/com/company/DataProcessor.java
@@ -47,20 +47,11 @@ public class DataProcessor {
      * is there a method that transforms one thing into another thing?
      */
     public static double findTheAverageAgeOfPeopleWhoDislikeMovies(List<Customer> customers, Movie targetMovie) {
-        List<Customer> customersWhoDislikeMovie = new ArrayList<>();
-
-        for (int i = 0; i < customers.size(); i++) {
-            if (customers.get(i).getDislikedMovies().contains(targetMovie)) {
-                customersWhoDislikeMovie.add(customers.get(i));
-            }
-        }
-
-        double sum = 0;
-        for (int i = 0; i < customersWhoDislikeMovie.size(); i++) {
-            sum += customersWhoDislikeMovie.get(i).getAge();
-        }
-
-        return sum / customersWhoDislikeMovie.size();
+        return customers.stream()
+                .filter(customer -> customer.getDislikedMovies().contains(targetMovie))
+                .map(Customer::getAge)
+                .mapToInt(Integer::intValue)
+                .average().orElse(0);
     }
 
     /**
